b5f45ec34346889ded432676508f1a126edb01d4
Andy Jennings <andy.jennings@interact-intranet.com>
2022-01-02 17:18:20 +0000

    Initial Commit

---
+++ PokePickrUITests/PokePickrUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PokePickrUITestsLaunchTests.swift
+//  PokePickrUITests
+//
+//  Created by Andrew Jennings on 02/01/2022.
+//
+
+import XCTest
+
+class PokePickrUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
